d3c14753ddb365fada37e49b90662940b7a6beeb
jzaldumbide <jzaldumbide@gmail.com>
2014-09-16 21:13:25 +1000

    last version testing

---
+++ webapp/test1/Endangered Animals_files/ie.js
@@ -0,0 +1,19 @@
+var ie = (function(){
+
+    var undef,
+        v = 3,
+        div = document.createElement('div'),
+        all = div.getElementsByTagName('i');
+
+    while (
+        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+        all[0]
+    );
+
+    return v > 4 ? v : undef;
+
+}());
+
+if (ie){
+  alert("Unfortunately Internet Explorer is not Supported. Please try the latest versions of Google Chrome, Safari or Firefox.")
+}
